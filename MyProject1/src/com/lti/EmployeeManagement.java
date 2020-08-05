a1c1cf484729579b0562e4e03e6bb712b017a6e2
Mayank Goel <mayank27goel@gmail.com>
2020-08-05 22:21:24 +0530

    New Project Added

---
+++ MyProject1/src/com/lti/EmployeeManagement.java
@@ -0,0 +1,15 @@
+package com.lti;
+
+public class EmployeeManagement {
+
+	public static void add(int a, int b){
+		
+		System.out.println("Addition= "+(a+b));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(5,3);
+	}
+
+}
