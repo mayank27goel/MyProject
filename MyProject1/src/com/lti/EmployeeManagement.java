a4c83e849c05217bb65a0a40e1bcba7a8455e33b
Mayank Goel <mayank27goel@gmail.com>
2020-08-06 09:47:22 +0530

    Subtraction method added

--- MyProject1/src/com/lti/EmployeeManagement.java
+++ MyProject1/src/com/lti/EmployeeManagement.java
@@ -7,9 +7,15 @@ public class EmployeeManagement {
 		System.out.println("Addition= "+(a+b));
 	}
 	
+public static void diff(int a, int b){
+		
+		System.out.println("Subtraction= "+(a-b));
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		add(5,3);
+		diff(10,5);
 	}
 
 }
